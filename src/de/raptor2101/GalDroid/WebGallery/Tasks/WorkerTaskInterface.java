dd60a80c892eeb80d889c62556aaad3a2bb137d6
Raptor 2101 <raptor2101@gmx.de>
2012-06-02 13:13:50 +0200

    ADD: WorkerTask

---
+++ src/de/raptor2101/GalDroid/WebGallery/Tasks/WorkerTaskInterface.java
@@ -0,0 +1,16 @@
+package de.raptor2101.GalDroid.WebGallery.Tasks;
+
+public interface WorkerTaskInterface {
+	public enum Status {
+		PENDING,
+		RUNNING,
+		SLEEPING,
+		FINISHED,
+		STOPPING
+	}
+	
+	public Status getStatus();
+	public void start();
+	public void stop();
+	public void cancel();
+}
