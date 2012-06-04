c74d2217ed724a07d11dc7e69fcde32653619e2b
Raptor 2101 <raptor2101@gmx.de>
2012-06-04 23:40:24 +0200

    CHG: COdeCleanup & replace tabs with spaces

--- src/de/raptor2101/GalDroid/WebGallery/Tasks/ImageInformationLoaderTask.java
+++ src/de/raptor2101/GalDroid/WebGallery/Tasks/ImageInformationLoaderTask.java
@@ -2,6 +2,7 @@ package de.raptor2101.GalDroid.WebGallery.Tasks;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.media.ExifInterface;
@@ -18,191 +19,196 @@ import de.raptor2101.GalDroid.WebGallery.Interfaces.GalleryObjectComment;
 import de.raptor2101.GalDroid.WebGallery.Interfaces.WebGallery;
 
 public class ImageInformationLoaderTask extends WorkerTask<GalleryObject, Void, Void> {
-    protected final static String CLASS_TAG = "ImageInformationLoaderTask";
-    public static final int MESSAGE_IMAGE_INFORMATION = 0;
-    public static final int MESSAGE_IMAGE_TAGS = 1;
-    public static final int MESSAGE_IMAGE_COMMENTS = 2;
+  private final static String CLASS_TAG = "ImageInformationLoaderTask";
+  public static final int MESSAGE_IMAGE_INFORMATION = 0;
+  public static final int MESSAGE_IMAGE_TAGS = 1;
+  public static final int MESSAGE_IMAGE_COMMENTS = 2;
 
-    private class MessageBody{
-	public final GalleryObject mGalleryObject;
-	public final Object mObject;
-	
-	public MessageBody(GalleryObject galleryObject, Object object){
-	    mGalleryObject = galleryObject;
-	    mObject = object;
-	}
+  private class MessageBody {
+    public final GalleryObject mGalleryObject;
+    public final Object mObject;
+
+    public MessageBody(GalleryObject galleryObject, Object object) {
+      mGalleryObject = galleryObject;
+      mObject = object;
     }
-    
-    private class InternHandler extends Handler {
-	@SuppressWarnings("unchecked")
-	@Override
-	public void handleMessage(Message msg) {
-	    ImageInformationLoaderTaskListener listener = mListener.get();
-	    MessageBody messsage = (MessageBody) msg.obj;
-	    if (listener != null) {
-		switch (msg.what) {
-		case ImageInformationLoaderTask.MESSAGE_IMAGE_INFORMATION:
-		    listener.onImageInformationLoaded(messsage.mGalleryObject, (ImageInformation) messsage.mObject);
+  }
 
-		    break;
-		case ImageInformationLoaderTask.MESSAGE_IMAGE_COMMENTS:
-		    listener.onImageCommetsLoaded(messsage.mGalleryObject, (List<GalleryObjectComment>) messsage.mObject);
-		    break;
-
-		case ImageInformationLoaderTask.MESSAGE_IMAGE_TAGS:
-		    listener.onImageTagsLoaded(messsage.mGalleryObject, (List<String>) messsage.mObject);
-		    break;
-
-		}
-	    }
-	}
-    }
-
-    private final InternHandler mResponseHandler = new InternHandler();
-    private final WebGallery mWebGallery;
-    private final ImageCache mCache;
-    private final WeakReference<ImageInformationLoaderTaskListener> mListener;
-
-    public ImageInformationLoaderTask(ImageInformationLoaderTaskListener listener, WebGallery webGallery, ImageCache cache) {
-	mThreadName = "ImageInformationLoaderTask";
-	mListener = new WeakReference<ImageInformationLoaderTaskListener>(listener);
-	mWebGallery = webGallery;
-	mCache = cache;
-    }
-
-    public void load(GalleryObject galleryObject) {
-	Log.d(CLASS_TAG,String.format("enqueuing %s for loading of ImageInformations",galleryObject));
-	enqueue(galleryObject);
-    }
-
+  private class InternHandler extends Handler {
+    @SuppressWarnings("unchecked")
     @Override
-    protected Void doInBackground(GalleryObject galleryObject) {
-	ImageInformation imageInformation = loadImageInformations(galleryObject);
-	
-	Message message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_INFORMATION, new MessageBody(galleryObject, imageInformation));
-	message.sendToTarget();
-	
-	if (!isCancelled()) {
-	    try {
-		Log.d(CLASS_TAG, String.format("Try to load tags for %s", galleryObject));
+    public void handleMessage(Message msg) {
+      ImageInformationLoaderTaskListener listener = mListener.get();
+      MessageBody messsage = (MessageBody) msg.obj;
+      if (listener != null) {
+        switch (msg.what) {
+        case ImageInformationLoaderTask.MESSAGE_IMAGE_INFORMATION:
+          listener.onImageInformationLoaded(messsage.mGalleryObject, (ImageInformation) messsage.mObject);
 
-		List<String> tags = mWebGallery.getDisplayObjectTags(galleryObject, null);
-		message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_TAGS, new MessageBody(galleryObject, tags));
-		message.sendToTarget();
-	    } catch (Exception e) {
-		Log.e(CLASS_TAG, String.format("Something goes wrong while loading tags for %s", galleryObject), e);
-	    }
-	}
-	
-	if (!isCancelled()) {
-        	try {
-        	    Log.d(CLASS_TAG,String.format("Try to load comments for %s", galleryObject));
-        	    List<GalleryObjectComment> comments = mWebGallery.getDisplayObjectComments(galleryObject, null);
-        	    message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_COMMENTS, new MessageBody(galleryObject, comments));
-        	    message.sendToTarget();
-        	} catch (Exception e) {
-        	    Log.e(CLASS_TAG,String.format("Something goes wrong while comments tags for %s", galleryObject),e);
-        	}
-	}
-	    
-	return null;
+          break;
+        case ImageInformationLoaderTask.MESSAGE_IMAGE_COMMENTS:
+          listener.onImageCommetsLoaded(messsage.mGalleryObject, (List<GalleryObjectComment>) messsage.mObject);
+          break;
+
+        case ImageInformationLoaderTask.MESSAGE_IMAGE_TAGS:
+          listener.onImageTagsLoaded(messsage.mGalleryObject, (List<String>) messsage.mObject);
+          break;
+
+        }
+      }
+    }
+  }
+
+  private final InternHandler mResponseHandler = new InternHandler();
+  private final WebGallery mWebGallery;
+  private final ImageCache mCache;
+  private final WeakReference<ImageInformationLoaderTaskListener> mListener;
+
+  public ImageInformationLoaderTask(ImageInformationLoaderTaskListener listener, WebGallery webGallery, ImageCache cache) {
+    mThreadName = "ImageInformationLoaderTask";
+    mListener = new WeakReference<ImageInformationLoaderTaskListener>(listener);
+    mWebGallery = webGallery;
+    mCache = cache;
+  }
+
+  public void load(GalleryObject galleryObject) {
+    if(galleryObject == null) {
+      return;
+    }
+    Log.d(CLASS_TAG, String.format("enqueuing %s for loading of ImageInformations", galleryObject));
+    enqueue(galleryObject);
+  }
+
+  @Override
+  protected Void doInBackground(GalleryObject galleryObject) {
+    ImageInformation imageInformation = loadImageInformations(galleryObject);
+
+    Message message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_INFORMATION, new MessageBody(galleryObject, imageInformation));
+    message.sendToTarget();
+
+    if (!isCancelled()) {
+      try {
+        Log.d(CLASS_TAG, String.format("Try to load tags for %s", galleryObject));
+
+        List<String> tags = mWebGallery.getDisplayObjectTags(galleryObject, null);
+        
+        message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_TAGS, new MessageBody(galleryObject, tags));
+        message.sendToTarget();
+      } catch (Exception e) {
+        Log.e(CLASS_TAG, String.format("Something goes wrong while loading tags for %s", galleryObject), e);
+      }
     }
 
-    private ImageInformation loadImageInformations(GalleryObject galleryObject) {
-	ImageInformation imageInformation = new ImageInformation();
-	
-	imageInformation.mTitle = galleryObject.getTitle();
-	imageInformation.mUploadDate = galleryObject.getDateUploaded();
-	    
-	File cachedFile = mCache.getFile(galleryObject.getImage().getUniqueId());
-	try {
-	    Log.d(CLASS_TAG,String.format("Try to decoding ExifInformations from %s", galleryObject));
-	    ExifInterface exif = new ExifInterface(cachedFile.getAbsolutePath());
-
-	    imageInformation.mExifCreateDate = exif.getAttribute(ExifInterface.TAG_DATETIME);
-	    imageInformation.mExifAperture = exif.getAttribute(ExifInterface.TAG_APERTURE);
-	    imageInformation.mExifIso = exif.getAttribute(ExifInterface.TAG_ISO);
-	    imageInformation.mExifModel = exif.getAttribute(ExifInterface.TAG_MODEL);
-	    imageInformation.mExifMake = exif.getAttribute(ExifInterface.TAG_MAKE);
-
-	    try {
-		imageInformation.mExifExposure = 1.0f / Float.parseFloat(exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME));
-
-	    } catch (Exception e) {
-		imageInformation.mExifExposure = 0f;
-	    }
-
-	    try {
-		imageInformation.mExifFlash = exif.getAttributeInt(ExifInterface.TAG_FLASH, 0);
-	    } catch (Exception e) {
-		imageInformation.mExifFlash = 0;
-	    }
-
-	    try {
-		imageInformation.mExifFocalLength = exif.getAttributeDouble(ExifInterface.TAG_FOCAL_LENGTH, 0);
-	    } catch (Exception e) {
-		imageInformation.mExifFocalLength = 0;
-	    }
-
-	    try {
-		int whiteBalance = exif.getAttributeInt(ExifInterface.TAG_FLASH, 0);
-		if (whiteBalance == ExifInterface.WHITEBALANCE_AUTO) {
-		    imageInformation.mExifWhiteBalance = WhiteBalance.Auto;
-		} else {
-		    imageInformation.mExifWhiteBalance = WhiteBalance.Manual;
-		}
-	    } catch (Exception e) {
-		imageInformation.mExifWhiteBalance = WhiteBalance.Manual;
-	    }
-
-	    imageInformation.mExifGpsLat = parseDegMinSec(exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE));
-	    imageInformation.mExifGpsLong = parseDegMinSec(exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE));
-	    imageInformation.mExifHeight = parseHeight(exif.getAttribute(ExifInterface.TAG_GPS_ALTITUDE));
-	} catch (Exception e) {
-	    Log.e(CLASS_TAG,String.format("Something goes wrong while decoding ExifInformations from %s", galleryObject),e);
-	}
-	return imageInformation;
+    if (!isCancelled()) {
+      try {
+        Log.d(CLASS_TAG, String.format("Try to load comments for %s", galleryObject));
+        List<GalleryObjectComment> comments = mWebGallery.getDisplayObjectComments(galleryObject, null);
+        
+        message = mResponseHandler.obtainMessage(MESSAGE_IMAGE_COMMENTS, new MessageBody(galleryObject, comments));
+        message.sendToTarget();
+      } catch (Exception e) {
+        Log.e(CLASS_TAG, String.format("Something goes wrong while comments tags for %s", galleryObject), e);
+      }
     }
 
-    private float parseHeight(String tagGpsValue) {
-	try {
-	    String[] values = tagGpsValue.split("/");
-	    return Float.parseFloat(values[0]) / Float.parseFloat(values[1]);
-	} catch (Exception e) {
-	    return 0;
-	}
-    }
+    return null;
+  }
 
-    private DegMinSec parseDegMinSec(String gpsValue) {
-	try {
-	    String[] values = gpsValue.split("[,/]");
-	    float deg = Float.parseFloat(values[0]) / Float.parseFloat(values[1]);
-	    float min = Float.parseFloat(values[2]) / Float.parseFloat(values[3]);
-	    float sec = Float.parseFloat(values[4]) / Float.parseFloat(values[5]);
-	    return new DegMinSec(deg, min, sec);
-	} catch (Exception e) {
-	    return new DegMinSec(0, 0, 0);
-	}
-    }
+  private ImageInformation loadImageInformations(GalleryObject galleryObject) {
+    ImageInformation imageInformation = new ImageInformation();
 
-    @Override
-    protected void onPostExecute(GalleryObject parameter, Void result) {
-	// TODO Auto-generated method stub
+    imageInformation.mTitle = galleryObject.getTitle();
+    imageInformation.mUploadDate = galleryObject.getDateUploaded();
 
-    }
-    public boolean isLoading(GalleryObject galleryObject) {
-	boolean isActive = isActive(galleryObject);
-	boolean isEnqueued = isEnqueued(galleryObject);
-	Log.d(CLASS_TAG, String.format("isActive: %s isEnqueued %s", isActive, isEnqueued));
-	return isActive || isEnqueued;
-    }
+    File cachedFile = mCache.getFile(galleryObject.getImage().getUniqueId());
+    try {
+      Log.d(CLASS_TAG, String.format("Try to decoding ExifInformations from %s", galleryObject));
+      ExifInterface exif = new ExifInterface(cachedFile.getAbsolutePath());
 
-    public void cancel(GalleryObject galleryObject) {
-	if(isEnqueued(galleryObject)) {
-	    removeEnqueued(galleryObject);
-	} else if (isActive(galleryObject)){
-	    cancelCurrent();
-	}
-	
+      imageInformation.mExifCreateDate = exif.getAttribute(ExifInterface.TAG_DATETIME);
+      imageInformation.mExifAperture = exif.getAttribute(ExifInterface.TAG_APERTURE);
+      imageInformation.mExifIso = exif.getAttribute(ExifInterface.TAG_ISO);
+      imageInformation.mExifModel = exif.getAttribute(ExifInterface.TAG_MODEL);
+      imageInformation.mExifMake = exif.getAttribute(ExifInterface.TAG_MAKE);
+
+      try {
+        imageInformation.mExifExposure = 1.0f / Float.parseFloat(exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME));
+
+      } catch (Exception e) {
+        imageInformation.mExifExposure = 0f;
+      }
+
+      try {
+        imageInformation.mExifFlash = exif.getAttributeInt(ExifInterface.TAG_FLASH, 0);
+      } catch (Exception e) {
+        imageInformation.mExifFlash = 0;
+      }
+
+      try {
+        imageInformation.mExifFocalLength = exif.getAttributeDouble(ExifInterface.TAG_FOCAL_LENGTH, 0);
+      } catch (Exception e) {
+        imageInformation.mExifFocalLength = 0;
+      }
+
+      try {
+        int whiteBalance = exif.getAttributeInt(ExifInterface.TAG_FLASH, 0);
+        if (whiteBalance == ExifInterface.WHITEBALANCE_AUTO) {
+          imageInformation.mExifWhiteBalance = WhiteBalance.Auto;
+        } else {
+          imageInformation.mExifWhiteBalance = WhiteBalance.Manual;
+        }
+      } catch (Exception e) {
+        imageInformation.mExifWhiteBalance = WhiteBalance.Manual;
+      }
+
+      imageInformation.mExifGpsLat = parseDegMinSec(exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE));
+      imageInformation.mExifGpsLong = parseDegMinSec(exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE));
+      imageInformation.mExifHeight = parseHeight(exif.getAttribute(ExifInterface.TAG_GPS_ALTITUDE));
+    } catch (Exception e) {
+      Log.e(CLASS_TAG, String.format("Something goes wrong while decoding ExifInformations from %s", galleryObject), e);
     }
+    return imageInformation;
+  }
+
+  private float parseHeight(String tagGpsValue) {
+    try {
+      String[] values = tagGpsValue.split("/");
+      return Float.parseFloat(values[0]) / Float.parseFloat(values[1]);
+    } catch (Exception e) {
+      return 0;
+    }
+  }
+
+  private DegMinSec parseDegMinSec(String gpsValue) {
+    try {
+      String[] values = gpsValue.split("[,/]");
+      float deg = Float.parseFloat(values[0]) / Float.parseFloat(values[1]);
+      float min = Float.parseFloat(values[2]) / Float.parseFloat(values[3]);
+      float sec = Float.parseFloat(values[4]) / Float.parseFloat(values[5]);
+      return new DegMinSec(deg, min, sec);
+    } catch (Exception e) {
+      return new DegMinSec(0, 0, 0);
+    }
+  }
+
+  @Override
+  protected void onPostExecute(GalleryObject parameter, Void result) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public boolean isLoading(GalleryObject galleryObject) {
+    boolean isActive = isActive(galleryObject);
+    boolean isEnqueued = isEnqueued(galleryObject);
+    Log.d(CLASS_TAG, String.format("isActive: %s isEnqueued %s", isActive, isEnqueued));
+    return isActive || isEnqueued;
+  }
+
+  public void cancel(GalleryObject galleryObject) {
+    if (isEnqueued(galleryObject)) {
+      removeEnqueued(galleryObject);
+    } else if (isActive(galleryObject)) {
+      cancelCurrent();
+    }
+  }
 }
