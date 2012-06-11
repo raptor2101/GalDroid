7a0c62f4dbaeada565cab19276ac01c116c34f7f
Raptor 2101 <raptor2101@gmx.de>
2012-06-11 21:39:19 +0200

    CHG: comment out currently unused lines of code

--- src/de/raptor2101/GalDroid/Activities/Listeners/ImageViewOnTouchListener.java
+++ src/de/raptor2101/GalDroid/Activities/Listeners/ImageViewOnTouchListener.java
@@ -93,10 +93,10 @@ public class ImageViewOnTouchListener implements OnTouchListener {
     float y = event.getY(0) - event.getY(1);
     return FloatMath.sqrt(x * x + y * y);
   }
-
-  private void setScalePoint(MotionEvent event) {
-    float x = event.getX(0) + event.getX(1);
-    float y = event.getY(0) + event.getY(1);
-    mScalePoint.set(x / 2, y / 2);
-  }
+// Comment out till needed  
+//  private void setScalePoint(MotionEvent event) {
+//    float x = event.getX(0) + event.getX(1);
+//    float y = event.getY(0) + event.getY(1);
+//    mScalePoint.set(x / 2, y / 2);
+//  }
 }
