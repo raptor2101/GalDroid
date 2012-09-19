65411bc3180b937c2da66487d0d2ee86bdf52c60
Raptor 2101 <raptor2101@gmx.de>
2012-09-19 23:40:46 +0200

    FIX: CodeCleanup

--- src/de/raptor2101/GalDroid/Activities/ImageViewActivity.java
+++ src/de/raptor2101/GalDroid/Activities/ImageViewActivity.java
@@ -29,7 +29,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
